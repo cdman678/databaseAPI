962d90733b4ea599bb8ef99a52abcc67c3330f61
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-26 11:18:15 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "databaseAPI.h"
+
+/*gcc -o exc $(mysql_config --cflags) main.c connect_and_close.c SQL_functions.c $(mysql_config --libs)*/
+
+int main(){
+  void description();
+return(0);
+}
