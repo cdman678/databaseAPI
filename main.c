7ebfb35297b148296fb7cfca1c36043efc13e09c
cdman678 <45344395+cdman678@users.noreply.github.com>
2019-03-26 11:18:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "databaseAPI.h"
 
-/*gcc -o exc $(mysql_config --cflags) main.c connect_and_close.c SQL_functions.c $(mysql_config --libs)*/
+/*gcc -o exc $(mysql_config --cflags) main.c plants_functions.c $(mysql_config --libs)*/
 
 int main(){
   void description();
